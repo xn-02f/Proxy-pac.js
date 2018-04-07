a50b8dff29f02fd3fd2144149de6f1b513399189
HuiyiFYJ <jxfengyijie@gmail.com>
2018-04-07 17:07:30 +0800

    Add rules about google
    As we know, .google belong to Google.
    We add ".google" rule in order to visite some domain, such as domains.google.
    
    Co-Authored-By: asscute <asscute@users.noreply.github.com>

--- proxy-pac.dev.js
+++ proxy-pac.dev.js
@@ -9,6 +9,7 @@ var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
   /* Google */
+  "||google",
   "\/^https?:\\\/\\\/[^\\\/]+google\\.(.*)\/",
   "||googleblog.com",
   "||g.co",
