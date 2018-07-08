0db0b10470ef345d8a5fd9ce240af323a9bfc7cb
HuiyiFYJ <jxfengyijie@gmail.com>
2018-07-09 00:42:20 +0800

    Remove 'colors' package
    Remove 'colors' package, because TravisCI console can't display its color.
    
    Co-Authored-By: P.S.H <asscute@users.noreply.github.com>

--- test/test.js
+++ test/test.js
@@ -1,12 +1,10 @@
 /*
  * This file is used to judge whether 'rules' is array type.
  */
-const colors = require('colors');
-
 const rules = require('./proxy-pac.test');
 
 const result = rules instanceof Array;
 
 if (result) {
-    console.log('\n  √ Test Pass...  \n'.green);
+    console.log('\n  √ Test Pass...  \n');
 }
